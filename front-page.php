ac22ca39506909809fd494dcd4152b91aa2f4235
jwmik123 <joelmik123@gmail.com>
2019-05-07 09:47:20 +0200

    Frontpage changes

--- front-page.php
+++ front-page.php
@@ -3,8 +3,8 @@
 
 <div class="container-fluid">
     <div class="row landing-header">
-        <div class="col landing-text">
-
+        <div class="col-md-1"></div>
+        <div class="col-md-5 landing-text">
             <div class="content">
                 <h2>Welcome to The Ultimate Portrait</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae, consequatur dignissimos, dolorum error et eveniet exercitationem facilis iusto quam quia quisquam sapiente. Expedita fugiat nemo quos unde. Iure, officiis!</p>
@@ -12,15 +12,29 @@
                 <iframe class="video" width="80%" height="375px" src="https://www.youtube.com/embed/XHxyqcg7dKA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
-        <div class="col landing-image">
+        <div class="col-md-6 landing-image">
             <!--Empty-->
         </div>
     </div>
+
+    <hr>
+    <div class="row portraits">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-11"><h2>Portraits</h2></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-3"><img src="https://via.placeholder.com/300" alt=""></div>
+        <div class="col-md-3"><img src="https://via.placeholder.com/300" alt=""></div>
+        <div class="col-md-3"><img src="https://via.placeholder.com/300" alt=""></div>
+        <div class="col-md-1"></div>
+    </div>
+    <hr>
+
     <div class="row about">
-        <div class="col about-image">
+        <div class="col-md-1"></div>
+        <div class="col-md-5 about-image">
             <img src="https://via.placeholder.com/450" alt="">
         </div>
-        <div class="col about-text">
+        <div class="col-md-5 about-text">
             <div class="content">
                 <h2>My name is Daniel Jeremy</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad deleniti exercitationem fugit harum itaque magnam officiis sapiente! Adipisci aliquid autem dolore itaque, nam optio praesentium tempore totam? A, et, laudantium.</p>
@@ -28,13 +42,7 @@
                 <button>contact</button>
             </div>
         </div>
-    </div>
-    <hr>
-    <div class="row portraits">
-        <div class="col-sm-12"><h2>Portraits</h2></div>
-        <div class="col-md-4"><img src="https://via.placeholder.com/300" alt=""></div>
-        <div class="col-md-4"><img src="https://via.placeholder.com/300" alt=""></div>
-        <div class="col-md-4"><img src="https://via.placeholder.com/300" alt=""></div>
+        <div class="col-md-1"></div>
     </div>
 
 
