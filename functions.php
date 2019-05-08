6db3ce76b303ca15edf3047aee3c61d3e2cb2274
basv1996 <bas.vugts@gmail.com>
2019-05-08 11:11:35 +0200

    contactPage + Form - no styling

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 function awesome_script_enqueue() {
      wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
      wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', 'true');
@@ -19,5 +21,8 @@ function awesome_theme_setup() {
 
 add_action('init', 'awesome_theme_setup');
 
-?>
 
+
+
+
+?>
