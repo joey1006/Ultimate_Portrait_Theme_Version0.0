60359f76279b3cdbefe22abb47194d16c3442fb3
basv1996 <bas.vugts@gmail.com>
2019-05-14 14:59:54 +0200

    navigation

--- header.php
+++ header.php
@@ -46,8 +46,7 @@
                   </div>
                   <div class="col-md-4">
                   </div>
-                </div>
-                <div class="row">
+
                     <div class="col-md-4"></div>
                       <div class="col-md-4"></div>
                   <div class="col-md-4">
